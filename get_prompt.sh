55fa79cf600d5bf888184e56c01968729ad35943
Tim Fournet <tfournet@radersolutions.com>
2021-03-04 20:49:58 -0600

    Update get_prompt.sh

--- get_prompt.sh
+++ get_prompt.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-# My URL is 
+# run me like this:
+# curl https://git.io/JqJzs | sh 
 curl -o .prompt.sh https://raw.githubusercontent.com/tfournet/configs_pub/main/bash_prompt.sh
 echo "source ~/.prompt.sh" >> ~/.bashrc
